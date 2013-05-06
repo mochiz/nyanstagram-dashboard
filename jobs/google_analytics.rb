751c077e4fb8d1b572b2220cf57773bdacd2b684
Hiroshi Asakura <mochi.hiro43@gmail.com>
2013-05-06 09:07:35 +0900

    add jobs/google_analytics

---
+++ jobs/google_analytics.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+SCHEDULER.every '15m', first_in: 0 do
+  # ユーザー数
+  send_event('user', GoogleAnalytics::NewVisits.get)
+
+  # アクセス数
+  send_event('access', GoogleAnalytics::AppViews.get)
+
+  # 検索TOP10
+  send_event('search_ranking', { items: GoogleAnalytics::SearchRankings.get })
+
+  # お気に入りTOP10
+  send_event('favorite_ranking', { items: GoogleAnalytics::FavRankings.get })
+end
